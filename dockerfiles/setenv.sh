57e0fd3a791fc1355797de5df0266f3e03571295
Patrik Björk <patrik.bjork@vgregion.se>
2021-12-11 15:33:22 +0100

    Set version to 5.0.5. Add -Dlog4j2.formatMsgNoLookups=true to JAVA_OPTS.

--- dockerfiles/setenv.sh
+++ dockerfiles/setenv.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Ending with dummy so we don't get any problems with windows line endings...
 CLASSPATH=$CLASSPATH:/opt/data/lakemedelsnara/properties:$CATALINA_HOME/log4j2/lib/*:$CATALINA_HOME/log4j2/conf:/dummy
+JAVA_OPTS="$JAVA_OPTS -Dlog4j2.formatMsgNoLookups=true"
